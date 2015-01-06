52dfbba81494a56d0d4dcd818bd1ed90b38322ac
FurutaYuki <furushchev@dhcp46.jsk.t.u-tokyo.ac.jp>
2015-01-06 17:29:36 +0900

    Initial Commit

---
+++ RobotMapViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RobotMapViewer
+//
+//  Created by FurutaYuki on 1/6/15.
+//  Copyright (c) 2015 JSK Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
